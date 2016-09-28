a7f7ae76dfecedc78e985161328018dfd78cee7d
oeno <soye73@naver.com>
2016-09-29 02:24:46 +0900

    1-5. MainPlayer.cs : the wall moves to left constantly

--- Assets/MainPlayer.cs
+++ Assets/MainPlayer.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class MainPlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    private GameObject wall;
+
+    // Use this for initialization
+    void Start () {
+        wall = GameObject.FindWithTag("WallSprite");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.Translate(3*Vector2.left * Time.deltaTime);
 	}
 }
