fa6c291c8e3a37662b239cd1f21db8b3c3abb74d
oeno <soye73@naver.com>
2016-09-29 03:26:39 +0900

    1-6. MainPlayer.cs : Add a feature - the wall is fade out

--- Assets/MainPlayer.cs
+++ Assets/MainPlayer.cs
@@ -4,10 +4,20 @@ using System.Collections;
 public class MainPlayer : MonoBehaviour {
     // Use this for initialization
     void Start () {
-	}
+        StartCoroutine("Fade");
+    }
 	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(3 * Vector2.left * Time.deltaTime);
 	}
+
+    IEnumerator Fade() {
+        for (float f=1f; f>=0; f-=0.2f) {
+            Color c = GetComponent<Renderer>().material.color;
+            c.a = f;
+            GetComponent<Renderer>().material.color = c;
+            yield return new WaitForSeconds(1f);
+        }
+    }
 }
