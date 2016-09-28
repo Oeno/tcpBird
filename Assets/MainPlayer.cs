f12fa763ae5f0266b0f358562c42bd84dbe30e8e
oeno <soye73@naver.com>
2016-09-29 03:01:33 +0900

    1-5. MainPlayer.cs : Correct a little

--- Assets/MainPlayer.cs
+++ Assets/MainPlayer.cs
@@ -2,16 +2,12 @@
 using System.Collections;
 
 public class MainPlayer : MonoBehaviour {
-
-    private GameObject wall;
-
     // Use this for initialization
     void Start () {
-        wall = GameObject.FindWithTag("WallSprite");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(3*Vector2.left * Time.deltaTime);
+        transform.Translate(3 * Vector2.left * Time.deltaTime);
 	}
 }
