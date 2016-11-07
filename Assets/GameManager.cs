c5d78917fc69b7cfa2f60de856a5525be2e64ba5
sonKrat <soye73@naver.com>
2016-11-07 16:44:41 +0900

    4-2. Display a score by Text UI

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
     public static GameManager Instance;
@@ -8,14 +9,17 @@ public class GameManager : MonoBehaviour {
     int score = 0;  // 점수
     bool isCollide = false; // 충돌 여부 확인
     float optSpeed = 0f;  // 벽에 시간에 따른 추가 속도 부여
+    Text scoreText;
     public Queue<GameObject> wallQueue = new Queue<GameObject>();
 
     void Awake() {
         Instance = this;
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
     }
 
     void Update()
     {
+        scoreText.text = "Score: " + score;
         optSpeed += Time.deltaTime;
     }
 
